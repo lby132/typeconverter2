f4e440dc264e52550ccb0b1cdb3828aac63396ff
lby132 <quddbscjswow@naver.com>
2022-05-01 18:23:02 +0900

    converter, formatter 사용

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,20 @@ public class HelloController {
     public String helloV1(HttpServletRequest request) {
         final String data = request.getParameter("data"); //문자 타입 조회
         final Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
-        System.out.println("intValue = " + intValue.equals(10));
+        System.out.println("intValue = " + intValue);
         return "ok";
     }
 
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) {
-        System.out.println("data = " + data.equals(10));
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort.getIp() = " + ipPort.getIp());
+        System.out.println("ipPort.getPort() = " + ipPort.getPort());
         return "ok";
     }
 }
